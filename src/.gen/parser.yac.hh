2087b8b787c110b241f57b9991a1bd4864f01f9b
Ivar Clemens <post@ivarclemens.nl>
2013-07-02 15:14:15 +0200

    .

---
+++ src/.gen/parser.yac.hh
@@ -0,0 +1,103 @@
+/* A Bison parser, made by GNU Bison 2.5.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     COLON = 258,
+     SETBYTEORDER = 259,
+     BIGENDIAN = 260,
+     LITTLEENDIAN = 261,
+     SENDCURRENTFRAME = 262,
+     SENDSTATUS = 263,
+     STREAMFRAMES = 264,
+     FREQUENCYDIVISOR = 265,
+     STOP = 266,
+     PROFILE = 267,
+     SET = 268,
+     EXECUTE = 269,
+     TABLE = 270,
+     NEXT = 271,
+     AFTER = 272,
+     BLEND = 273,
+     BEFORE = 274,
+     SINUSOID = 275,
+     START = 276,
+     LIGHTS = 277,
+     ON = 278,
+     OFF = 279,
+     BYE = 280,
+     HOME = 281,
+     CLEARFAULT = 282,
+     SETINTERNALSTATUS = 283,
+     PREOPERATIONAL = 284,
+     OPERATIONAL = 285,
+     OUTPUTENABLED = 286,
+     SENDINTERNALSTATUS = 287,
+     POSTYPE = 288,
+     INT = 289,
+     FLOAT = 290,
+     STRING = 291
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 2068 of yacc.c  */
+#line 21 "parser.yac"
+
+  int ival;
+  float fval;
+  char *sval;
+  position_type_t pval;
+
+
+
+/* Line 2068 of yacc.c  */
+#line 95 ".gen/parser.yac.hh"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+
+
