7f69eed8c045dc45017abd1073a36f61cddb6d35
Ivar Clemens <post@ivarclemens.nl>
2013-07-02 14:09:32 +0200

    First step in integration of sled_test.cc and sled.cc

--- src/sled.cc
+++ src/sled.cc
@@ -4,6 +4,130 @@
 #include <stdio.h>
 
 
+/** ******************
+ * Callback functions
+ ****************** **/
+
+
+/**
+ * Inform interface state machine that interface has closed.
+ */
+void intf_on_close(intf_t *intf, void *payload)
+{
+	sled_t *sled = (sled_t *) payload;
+	mch_intf_handle_event(sled->mch_intf, EV_INTF_CLOSE);
+}
+
+
+/**
+ * Handle network management state notification.
+ *
+ * @param intf  Interface that changed state
+ * @param payload  Void pointer to state machine struct
+ * @param state  New state
+ */
+void intf_on_nmt(intf_t *intf, void *payload, uint8_t state)
+{
+	sled_t *sled = (sled_t *) payload;
+
+	switch(state) {
+		case 0x04: mch_net_handle_event(sled->mch_net, EV_NET_STOPPED); break;
+		case 0x05: mch_net_handle_event(sled->mch_net, EV_NET_OPERATIONAL); break;
+		case 0x7F: mch_net_handle_event(sled->mch_net, EV_NET_PREOPERATIONAL); break;
+	}
+}
+
+
+/**
+ * Handle write (SDO) response.
+ */
+void intf_on_write_response(intf_t *intf, void *payload, uint16_t index, uint8_t subindex)
+{
+	sled_t *sled = (sled_t *) payload;
+	mch_sdo_handle_event(sled->mch_sdo, EV_SDO_READ_RESPONSE);
+}
+
+
+/**
+ * Handle read (SDO) response.
+ */
+void intf_on_abort_response(intf_t *intf, void *payload, uint16_t index, uint8_t subindex, uint32_t abort)
+{
+	sled_t *sled = (sled_t *) payload;
+	mch_sdo_handle_event(sled->mch_sdo, EV_SDO_ABORT_RESPONSE);
+}
+
+
+/**
+ * Handle TPDO.
+ */
+void intf_on_tpdo(intf_t *intf, void *payload, int pdo, uint8_t *data)
+{
+	sled_t *sled = (sled_t *) payload;
+
+	if(pdo == 1) {
+		uint16_t status = (data[1] << 8) | data[0];
+		uint8_t mode = data[2];
+
+		if((status & 0x4F) == 0x40) mch_ds_handle_event(sled->mch_ds, EV_DS_NOT_READY_TO_SWITCH_ON);
+		if((status & 0x6F) == 0x21) mch_ds_handle_event(sled->mch_ds, EV_DS_READY_TO_SWITCH_ON);
+		if((status & 0x6F) == 0x23) mch_ds_handle_event(sled->mch_ds, EV_DS_SWITCHED_ON);
+		if((status & 0x6F) == 0x27) mch_ds_handle_event(sled->mch_ds, EV_DS_OPERATION_ENABLED);
+		if((status & 0x4F) == 0x08) mch_ds_handle_event(sled->mch_ds, EV_DS_FAULT);
+		if((status & 0x4F) == 0x0F) mch_ds_handle_event(sled->mch_ds, EV_DS_FAULT_REACTION_ACTIVE);
+		if((status & 0x6F) == 0x07) mch_ds_handle_event(sled->mch_ds, EV_DS_QUICK_STOP_ACTIVE);
+
+		if((status & 0x10) == 0x10)
+			mch_ds_handle_event(sled->mch_ds, EV_DS_VOLTAGE_ENABLED);
+		else
+			mch_ds_handle_event(sled->mch_ds, EV_DS_VOLTAGE_DISABLED);
+
+		if((status & 0x1000) == 0x1000)
+			mch_mp_handle_event(sled->mch_mp, EV_MP_HOMED);
+		else
+			mch_mp_handle_event(sled->mch_mp, EV_MP_NOTHOMED);
+
+		if(mode == 0x06)
+			mch_mp_handle_event(sled->mch_mp, EV_MP_MODE_HOMING);
+		if(mode == 0x08)
+			mch_mp_handle_event(sled->mch_mp, EV_MP_MODE_PP);
+
+		//printf("Status: %04x\tMode: %02x\n", status, mode);
+	}
+
+	if(pdo == 2) {
+		int32_t position = (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0];
+		int32_t velocity = (data[7] << 24) | (data[6] << 16) | (data[5] << 8) | data[4];
+
+		//printf("Position: %d\tVelocity: %d\n", position, velocity);
+	}
+}
+
+// Inform network interface that the interface has opened / closed.
+CALLBACK_FUNCTION_EVENT(intf, on_opened, net, EV_NET_INTF_OPENED)
+CALLBACK_FUNCTION_EVENT(intf, on_closed, net, EV_NET_INTF_CLOSED)
+
+// Inform SDO machine that SDO transmission is (not) possible.
+CALLBACK_FUNCTION_EVENT(net, on_sdos_enabled, sdo, EV_NET_SDO_ENABLED)
+CALLBACK_FUNCTION_EVENT(net, on_sdos_disabled, sdo, EV_NET_SDO_DISABLED)
+
+// Notify DS402 that NMT is (in)operational.
+CALLBACK_FUNCTION_EVENT(net, on_enter_operational, ds, EV_DS_NET_OPERATIONAL);
+CALLBACK_FUNCTION_EVENT(net, on_leave_operational, ds, EV_DS_NET_INOPERATIONAL);
+
+// Inform NMT machine that SDO queue is empty.
+CALLBACK_FUNCTION_EVENT(sdo, on_queue_empty, net, EV_NET_SDO_QUEUE_EMPTY);
+
+// Inform MP machine that DS is (in)operational.
+CALLBACK_FUNCTION_EVENT(ds, on_operation_enabled, mp, EV_MP_DS_OPERATIONAL);
+CALLBACK_FUNCTION_EVENT(ds, on_operation_disabled, mp, EV_MP_DS_INOPERATIONAL);
+
+
+/** *******************************
+ * Implementation of API functions
+ ******************************* **/
+
+
 /**
  * Setup sled structures.
  */
@@ -15,6 +139,25 @@ sled_t *sled_create()
   for(int profile = 0; profile < MAX_PROFILES; profile++)
     sled_profile_clear(sled, profile, false);
 
+	// Register interface callback functions
+	intf_set_close_handler(sled->interface, intf_on_close);
+	intf_set_nmt_state_handler(sled->interface, intf_on_nmt);
+	intf_set_write_resp_handler(sled->interface, intf_on_write_response);
+	intf_set_abort_resp_handler(sled->interface, intf_on_abort_response);
+	intf_set_tpdo_handler(sled->interface, intf_on_tpdo);
+
+	// Register callback functions for state machines.
+	REGISTER_CALLBACK(intf, opened);
+	REGISTER_CALLBACK(intf, closed);
+	REGISTER_CALLBACK(net, sdos_enabled);
+	REGISTER_CALLBACK(net, sdos_disabled);
+	REGISTER_CALLBACK(net, enter_operational);
+	REGISTER_CALLBACK(net, leave_operational);
+	REGISTER_CALLBACK(sdo, queue_empty);
+	REGISTER_CALLBACK(ds, operation_enabled);
+	REGISTER_CALLBACK(ds, operation_disabled);
+
+
   return sled;
 }
 
@@ -22,7 +165,7 @@ sled_t *sled_create()
 void sled_destroy(sled_t **handle)
 {
   sled_t *sled = *handle;
-  
+
   free(*handle);
   *handle = NULL;
 }
--- src/sled_internal.h
+++ src/sled_internal.h
@@ -4,23 +4,57 @@
 #include "sled.h"
 #include "sled_profile.h"
 
+#include "interface.h"
+
+#include "machines/mch_intf.h"
+#include "machines/mch_net.h"
+#include "machines/mch_sdo.h"
+#include "machines/mch_ds.h"
+#include "machines/mch_mp.h"
+
 #define MAX_PROFILES 100
 
+/**
+ * Generates callback function for callback FNAME of the SNAME machine.
+ * When executed it sends event EVENT to the DNAME machine.
+ */
+#define CALLBACK_FUNCTION_EVENT(SNAME, FNAME, DNAME, EVENT) \
+	void mch_ ## SNAME ## _ ## FNAME (mch_ ## SNAME ## _t *mch_ ## SNAME, void *payload) { \
+	sled_t *sled = (sled_t *) payload; \
+	mch_ ## DNAME ## _handle_event(sled->mch_ ## DNAME, EVENT); }
+
+/**
+ * Registers callback function FNAME with state machine SNAME.
+ */
+#define REGISTER_CALLBACK(SNAME, FNAME) \
+	mch_ ## SNAME ## _set_ ## FNAME ## _handler(sled->mch_ ## SNAME, mch_ ## SNAME ## _on_ ## FNAME);
+
 struct sled_profile_t {
-  bool in_use;
-  int profile;
+	bool in_use;
+	int profile;
 
-  int table;
-  position_type_t position_type;
-  double position, time;
+	int table;
+	position_type_t position_type;
+	double position, time;
 
-  int next_profile;
-  double delay;
-  blend_type_t blend_type;
+	int next_profile;
+	double delay;
+	blend_type_t blend_type;
 };
 
 struct sled_t {
-  sled_profile_t profiles[MAX_PROFILES];
+	// Local copy of motion profiles
+	sled_profile_t profiles[MAX_PROFILES];
+
+	// CANOpen interface
+	intf_t *interface;
+
+	// State machines
+	mch_intf_t *mch_intf;
+	mch_net_t *mch_net;
+	mch_sdo_t *mch_sdo;
+	mch_ds_t *mch_ds;
+	mch_mp_t *mch_mp;
 };
 
 void sled_profile_clear(sled_t *sled, int profile, bool in_use);
