47fd22728f5180387df4200c8e76291b1a6cb517
Ivar Clemens <post@ivarclemens.nl>
2013-07-03 08:39:16 +0200

    Whitespace

--- src/machines/mch_intf.cc
+++ src/machines/mch_intf.cc
@@ -15,22 +15,22 @@ mch_intf_state_t mch_intf_next_state_given_event(mch_intf_t *machine, mch_intf_e
   switch(machine->state) {
     case ST_INTF_CLOSED:      
       if(event == EV_INTF_OPEN)
-	return ST_INTF_OPENING;      
+				return ST_INTF_OPENING;      
       break;
       
     case ST_INTF_OPENING:
       if(event == EV_INTF_OPENED)
-	return ST_INTF_OPENED;
+				return ST_INTF_OPENED;
       break;
     
     case ST_INTF_CLOSING:
       if(event == EV_INTF_CLOSED)
-	return ST_INTF_CLOSED;
+				return ST_INTF_CLOSED;
       break;
 
     case ST_INTF_OPENED:
       if(event == EV_INTF_CLOSE)
-	return ST_INTF_CLOSING;
+				return ST_INTF_CLOSING;
       break;      
   }
   
@@ -43,9 +43,9 @@ void mch_intf_on_enter(mch_intf_t *machine)
   switch(machine->state) {
     case ST_INTF_OPENING:
       if(intf_open(machine->interface) != 0)
-	mch_intf_handle_event(machine, EV_INTF_CLOSED);
+				mch_intf_handle_event(machine, EV_INTF_CLOSED);
       else
-	mch_intf_handle_event(machine, EV_INTF_OPENED);
+				mch_intf_handle_event(machine, EV_INTF_OPENED);
       break;
       
     case ST_INTF_CLOSING:
@@ -53,16 +53,16 @@ void mch_intf_on_enter(mch_intf_t *machine)
       mch_intf_handle_event(machine, EV_INTF_CLOSED);
       break;
 
-	case ST_INTF_OPENED:
-		if(machine->opened_handler)
-			machine->opened_handler(machine, machine->payload);
-		break;
+		case ST_INTF_OPENED:
+			if(machine->opened_handler)
+				machine->opened_handler(machine, machine->payload);
+			break;
 
-	case ST_INTF_CLOSED:
-		if(machine->closed_handler)
-			machine->closed_handler(machine, machine->payload);
-		break;
-  }
+		case ST_INTF_CLOSED:
+			if(machine->closed_handler)
+				machine->closed_handler(machine, machine->payload);
+			break;
+	 }
 }
 
 
