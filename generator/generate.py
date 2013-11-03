ccfec56be7aad584e7ee7b513b79dd887c0e2b96
Ivar Clemens <post@ivarclemens.nl>
2013-11-03 12:19:25 +0100

    Clean-up generator script.

--- generator/generate.py
+++ generator/generate.py
@@ -17,22 +17,33 @@ def generate_header(names, machine):
   argument_list = string.join(fields, ', ')
 
   header = \
-    "#ifndef __MACHINE_{0}_H__\n\n" \
-    "/* This file was automatically generated. Do not modify. */\n\n" \
-    "{8}\n" \
-    "struct {2};\n\n" \
-    "enum {3} {{\n\t{6}\n" \
-    "}};\n\n" \
-    "enum {4} {{\n\t{7}\n" \
-    "}};\n\n" \
-    "{2} *{1}_create({5});\n" \
-    "void {1}_destroy({2} **machine);\n" \
-    "{3} {1}_active_state({2} *machine);\n" \
-    "void {1}_handle_event({2} *machine, {4} event);\n" \
-    "void {1}_set_callback_payload({2} *machine, {4} event);\n" \
-    "{3} {1}_next_state_given_event({2} *machine, {4} event);\n" \
-    "void {1}_on_enter({2} *machine);\n" \
-    "void {1}_on_exit({2} *machine);\n\n" \
+"""#ifndef __MACHINE_{0}_H__
+
+/* This file was automatically generated. Do not modify. */
+
+{8}
+
+struct {2};
+
+enum {3} {{
+  {6}
+}};
+
+enum {4} {{
+  {7}
+}};
+
+{2} *{1}_create({5});
+void {1}_destroy({2} **machine);
+
+void {1}_on_entry({2} *machine);
+void {1}_on_exit({2} *machine);
+
+void {1}_handle_event({2} *machine, {4} event);
+{3} {1}_next_state_given_event({2} *machine, {4} event);
+{3} {1}_active_state({2} *machine);
+
+void {1}_set_callback_payload({2} *machine, {4} event);""" \
     .format(
       machine['name'].upper(),  #0
       prefix,                   #1
@@ -48,6 +59,7 @@ def generate_header(names, machine):
   for callback in machine['callbacks']:
     header += "typedef void(*{}_{}_t)({} *machine, void *payload);\n".format(
       prefix, callback, machine_type)
+  header += "\n"
 
   for callback in machine['callbacks']:
     header += "void {0}_set_{1}({2} *machine, {0}_{1}_t handler);\n".format(
@@ -118,35 +130,52 @@ def generate_func_eventname(names, machine):
   return func
 
 
-def generate_func_handle_event(names, machine):
-  func = "void {0[prefix]}_handle_event({0[machine_type]} *machine, {0[event_type]} event)\n" \
-         "{{\n" \
-         "\tassert(machine);\n" \
-         "\t{0[state_type]} next_state = {0[prefix]}_next_state_given_event(machine, event);\n" \
-         "\tif(!machine->state == next_state) {{\n" \
-         "\t\t{0[prefix]}_on_exit(machine);\n" \
-         "\t\tmachine->state = next_state;\n" \
-         "\t\tsyslog(LOG_DEBUG, \"%s() state changed to %s\", __FUNCTION__, {0[prefix]}_statename(machine->state));\n" \
-         "\t\t{0[prefix]}_on_enter(machine);\n" \
-         "\t}}\n" \
-         "}}\n".format(names);
-  return func
+#
+# Function that needs to be called when an event occurs.
+# It checks whether a transition is required and executes it.
+#
+def function_handle_event(names, machine):
+  return \
+"""/**
+ * Receives events and performs state transition if required.
+ */
+void {0[prefix]}_handle_event({0[machine_type]} *machine, {0[event_type]} event)
+{{
+  assert(machine);
+  {0[state_type]} next_state = {0[prefix]}_next_state_given_event(machine, event);
+
+  if(!machine->state == next_state) {{
+    {0[prefix]}_on_exit(machine);
+    machine->state = next_state;
+    syslog(LOG_DEBUG, \"%s() state changed to %s\", 
+      __FUNCTION__, {0[prefix]}_statename(machine->state));
+    {0[prefix]}_on_entry(machine);
+  }}
+}}
+""".format(names)
   
 
-def generate_func_create(names, machine):
+#
+# Function used to setup state machine structure.
+#
+def function_create_machine(names, machine):
   fields = list()
   for field in machine['fields']:
     fields.append(field[0] + ' ' + field[1])
   argument_list = string.join(fields, ', ')
 
+  func = \
+"""/**
+ * Setup state machine structure.
+ */
+{0[machine_type]} *{0[prefix]}_create({1})
+{{
+  {0[machine_type]} *machine = new {0[machine_type]};
   
-  func = "{0[machine_type]} *{0[prefix]}_create({1})\n" \
-         "{{\n" \
-         "\t{0[machine_type]} *machine = new {0[machine_type]};\n" \
-         "\t\n" \
-         "\tmachine->state = {2};\n" \
-         "\tmachine->payload = NULL;\n" \
-         "\t\n".format(names, argument_list, machine['initial'])
+  machine->state = {2};
+  machine->payload = NULL;
+
+""".format(names, argument_list, machine['initial'])
 
   for field in machine['fields']:
     func += "\tmachine->{0} = {0};\n".format(field[1])
@@ -156,24 +185,34 @@ def generate_func_create(names, machine):
     func += "\tmachine->{0}_handler = NULL;\n".format(callback)
   func += "\n"
 
-  func += "\tassert(machine);\n" \
-          "\treturn machine;\n" \
-          "}\n"
+  func += """
+  assert(machine);
+  return machine;
+}
+"""
+
   return func
   
 
-def generate_func_destroy(names, machine):
-  func = "void {0[prefix]}_destroy({0[machine_type]} **machine)\n" \
-         "{{\n" \
-         "\tfree(*machine);\n" \
-         "\t*machine = NULL;\n" \
-         "}}\n".format(names)
-  return func  
+#
+# Function used to free state machine structure.
+#
+def function_destroy_machine(names, machine):
+  return \
+"""/**
+ * Free state machine structure.
+ */
+void {0[prefix]}_destroy({0[machine_type]} **machine)
+{{
+  free(*machine);
+  *machine = NULL;
+}}
+""".format(names)
 
 
 
-def generate_func_on_enter(names, machine):
-  func = "void {0[prefix]}_on_enter({0[machine_type]} *machine)\n" \
+def generate_func_on_entry(names, machine):
+  func = "void {0[prefix]}_on_entry({0[machine_type]} *machine)\n" \
          "{{\n" \
          "\tswitch(machine->state)\n" \
          "\t{{\n".format(names)
@@ -228,6 +267,10 @@ def generate_body(names, machine):
 
   body += generate_struct_def(names, machine) + "\n"
   body += machine['source'] + "\n"
+  
+  body += function_create_machine(names, machine) + "\n"  
+  body += function_destroy_machine(names, machine) + "\n"  
+  
   body += generate_func_set_callback_payload(names, machine) + "\n"
   
   for callback in machine['callbacks']:
@@ -236,13 +279,11 @@ def generate_body(names, machine):
   body += generate_func_statename(names, machine) + "\n"
   body += generate_func_eventname(names, machine) + "\n"  
 
-  body += generate_func_on_enter(names, machine) + "\n"
+  body += generate_func_on_entry(names, machine) + "\n"
   body += generate_func_on_exit(names, machine) + "\n"
 
-  body += generate_func_handle_event(names, machine) + "\n"
+  body += function_handle_event(names, machine) + "\n"
   
-  body += generate_func_create(names, machine) + "\n"  
-  body += generate_func_destroy(names, machine) + "\n"
   body += generate_func_active_state(names, machine) + "\n"
 
   
