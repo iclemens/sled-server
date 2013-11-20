b6b2b301db44ae03b096ef6c049cfcb096a690ba
Ivar Clemens <post@ivarclemens.nl>
2013-11-20 16:29:43 +0100

    Cleaned up generator code.

--- generator/genmch.py
+++ generator/genmch.py
@@ -6,167 +6,191 @@
 import sys
 import xml.etree.ElementTree as ElementTree
 
+
+###################################
+# Functions for header generation #
+###################################
+
+
+def enum_definition(name, values):
+  enum = "enum {} {{\n".format(name)
+  for value in values:
+    enum += "\t{},\n".format(value)
+  return enum + "};\n\n"
+
+
+def compile_header(data):
+  header = ""
+  prefix = data.attrib['prefix']
+
+  header = "// WARNING: Automatically generated file! Do not modify!\n" \
+           "#ifndef __{1}_H__\n" \
+           "#define __{1}_H__\n\n" \
+           "struct {0}_t;\n" \
+           "\n".format(prefix, prefix.upper())
+
+  header += enum_definition("{}_event_t".format(prefix), [e.text for e in data.findall('./events/event')])
+  header += enum_definition("{}_state_t".format(prefix), [e.text for e in data.findall('./states/state')])
+
+  for element in data.findall("./include"):
+    header += element.text + "\n"
+
+  args = ", ".join([e.attrib['type'] + e.text for e in data.findall("./fields/field")])
+
+  header += "{0}_t *{0}_create({1});\n" \
+            "void {0}_destroy({0}_t **machine);\n" \
+            "{0}_state_t {0}_active_state({0}_t *machine);\n" \
+            "void {0}_handle_event({0}_t *machine, {0}_event_t event);\n" \
+            "void {0}_set_callback_payload({0}_t *machine, void *payload);\n" \
+            "\n".format(prefix, args)
+
+  for element in data.findall("./callbacks/callback"):
+    header += "typedef void(*{0}_{1}_handler_t)({0}_t *machine, void *payload);\n" \
+              "void {0}_set_{1}_handler({0}_t *machine, {0}_{1}_handler_t handler);\n" \
+              "\n".format(prefix, element.text)
+
+  return header + "#endif\n\n"
+
+
+###################################
+# Functions for source generation #
+###################################
+
+
+def function_enum_to_string(fname, ename, values):
+  source = "const char *{0}({1} val)\n{{\n" \
+           "\tswitch(val) {{\n".format(fname, ename)
+  for value in values:
+    source += "\t\tcase {0}: return \"{0}\";\n".format(value)
+  return source + "\t}\n\treturn \"Value not in enum.\";\n}\n\n"
+
+
+def function_create(data, forward = False):
+  prefix = data.attrib['prefix']
+  args = ", ".join([e.attrib['type'] + e.text for e in data.findall("./fields/field")])
+
+  func = "{0}_t *{0}_create({1})".format(prefix, args)
+  if forward:
+    return func + ";\n"
+
+  func += "\n{\n"
+  func += "\t{0}_t *machine = new {0}_t();\n\n".format(prefix)
+  func += "\tmachine->state = {0};\n".format(data.findall("initial")[0].text)
+  func += "\tmachine->payload = NULL;\n\n"
+
+  for element in data.findall("./fields/field"):
+    func += "\tmachine->{0} = {0};\n".format(element.text)
+  func += "\n"
+
+  for element in data.findall("./fields/field_decl"):
+    if 'init' in element.attrib:
+      func += "\tmachine->{0} = {1};\n".format(element.text, element.attrib['init'])
+  func += "\n"
+
+  for element in data.findall("./callbacks/callback"):
+    func += "\tmachine->{0}_handler = NULL;\n".format(element.text)
+
+  return func + "}\n\n"
+
+
+# Generates a definition for the state-machine structure
+def structure_definition(data):
+  prefix = data.attrib['prefix']
+  struct = "struct {0}_t {{\n" \
+           "\t{0}_state_t state;\n" \
+           "\tvoid *payload;\n\n".format(prefix)
+
+  for element in data.findall("./callbacks/callback"):
+    struct += "\t{0}_{1}_handler_t {1}_handler;\n".format(prefix, element.text)
+  for element in data.findall("./fields/field"):
+    struct += "\t{0} {1};\n".format(element.attrib['type'], element.text)
+  for element in data.findall("./fields/field_decl"):
+    struct += "\t{0} {1};\n".format(element.attrib['type'], element.text)
+
+  return struct + "};\n\n"
+
+
+def compile_source(data):
+  prefix = data.attrib['prefix']
+
+  source = "// WARNING: Automatically generated file! Do not modify!\n" \
+           "#include \"{0}.h\"\n" \
+           "#include <assert.h>\n" \
+           "#include <stdlib.h>\n" \
+           "#include <syslog.h>\n" \
+           "\n".format(prefix)
+
+  source += structure_definition(root)
+
+  # Forward declarations
+  source += "static {0}_state_t {0}_next_state_given_event({0}_t *machine, {0}_event_t event);\n" \
+            "static void {0}_on_enter({0}_t *machine);\n" \
+            "static void {0}_on_exit({0}_t *machine);\n" \
+            "\n".format(prefix)
+
+  source += function_create(root)
+
+  # Destroy function
+  source += "void {0}_destroy({0}_t **machine)\n{{\n" \
+            "\tfree(*machine);\n" \
+            "\t*machine = NULL;\n" \
+            "}}\n\n".format(prefix)
+
+  # Active state
+  source += "{0}_state_t {0}_active_state({0}_t *machine)\n{{\n" \
+            "\tassert(machine);\n" \
+            "\treturn machine->state;\n" \
+            "}}\n\n".format(prefix)
+
+  # Callback payload
+  source += "void {0}_set_callback_payload({0}_t *machine, void *payload)\n{{\n" \
+            "\tmachine->payload = payload;\n" \
+            "}}\n\n".format(prefix)
+
+  for element in data.findall("./callbacks/callback"):
+    source += "void {0}_set_{1}_handler({0}_t *machine, {0}_{1}_handler_t handler)\n{{\n" \
+              "\tmachine->{1}_handler = handler;\n" \
+              "}}\n\n".format(prefix, element.text)
+
+  source += function_enum_to_string(prefix + "_statename", prefix + "_state_t", [e.text for e in data.findall('./states/state')])
+  source += function_enum_to_string(prefix + "_eventname", prefix + "_event_t", [e.text for e in data.findall('./events/event')])
+  
+  # Handle event
+  source += "void {0}_handle_event({0}_t *machine, {0}_event_t event)\n{{\n" \
+            "\tassert(machine);\n" \
+            "\t{0}_state_t next_state = {0}_next_state_given_event(machine, event);\n" \
+            "\tif(!(machine->state == next_state)) {{\n" \
+            "\t\t{0}_on_exit(machine);\n" \
+            "\t\tmachine->state = next_state;\n" \
+            "\t\tsyslog(LOG_DEBUG, \"%s() state changed to %s\",  __FUNCTION__, {0}_statename(machine->state));\n" \
+            "\t\t{0}_on_enter(machine);\n" \
+            "\t}}\n" \
+            "}}\n\n".format(prefix)
+
+  return source
+
+
+#################
+# Main function #
+#################
+
+
 if len(sys.argv) < 4:
   print "Invalid argument count. Usage: python genmch.py SOURCE.XML TARGET.C TARGET.H"
 
-source = sys.argv[1]
-cfile = open(sys.argv[2], 'w')
-hfile = open(sys.argv[3], 'w')
-
-if not cfile or not hfile:
-  print "Could not open output files"
-
 try:
-  root = ElementTree.parse(source).getroot()
+  root = ElementTree.parse(sys.argv[1]).getroot()
 except Exception as e:
   print "Could not parse input XML: " + str(e.message)
   sys.exit(1)
 
-prefix = root.attrib['prefix']
+# Write header file
+hfile = open(sys.argv[3], 'w')
+hfile.write(compile_header(root))
 
+# Write source file
+cfile = open(sys.argv[2], 'w')
+cfile.write(compile_source(root))
 
-def generate_enum(f, name, values):
-  f.write("enum {} {{\n".format(name))
-  for value in values:
-    f.write("\t{},\n".format(value))
-  f.write("};\n\n")
-
-
-def generate_header(f, data):
-  prefix = data.attrib['prefix']
-
-  f.write("// WARNING: Automatically generated file! Do not modify!\n" \
-          "#ifndef __{1}_H__\n" \
-          "#define __{0}_H__\n\n" \
-          "struct {0}_t;\n" \
-          "\n".format(prefix, prefix.upper()))
-
-  generate_enum(f, "{}_event_t".format(prefix), [e.text for e in data.findall('./events/event')])
-  generate_enum(f, "{}_state_t".format(prefix), [e.text for e in data.findall('./states/state')])
-
-  for element in data.findall("./include"):
-    f.write(element.text + "\n")
-
-  args = ", ".join([e.attrib['type'] + e.text for e in data.findall("./fields/field")])
-
-  f.write("{0}_t *{0}_create({1});\n" \
-          "void {0}_destroy({0}_t **machine);\n" \
-          "{0}_state_t {0}_active_state({0}_t *machine);\n" \
-          "void {0}_handle_event({0}_t *machine, {0}_event_t event);\n" \
-          "void {0}_set_callback_payload({0}_t *machine, void *payload);\n" \
-          "\n".format(prefix, args))  
-
-  for element in data.findall("./callbacks/callback"):
-    f.write("typedef void(*{0}_{1}_handler_t)({0}_t *machine, void *payload);\n".format(prefix, element.text))
-    f.write("void {0}_set_{1}_handler({0}_t *machine, {0}_{1}_handler_t handler);\n".format(prefix, element.text))
-    f.write("\n")
-
-  f.write("#endif\n\n")
-
-generate_header(hfile, root)
-
-####################
-# SOURCE GENERATOR #
-####################
-
-cfile.write("// WARNING: Automatically generated file! Do not modify!\n")
-cfile.write("#include \"{0}.h\"\n".format(prefix))
-cfile.write("#include <assert.h>\n")
-cfile.write("#include <stdlib.h>\n")
-cfile.write("#include <syslog.h>\n\n")
-
-# Structure
-cfile.write("struct {0}_t {{\n".format(prefix))
-cfile.write("\t{0}_state_t state;\n".format(prefix))
-cfile.write("\tvoid *payload;\n\n".format(prefix))
-for element in root.findall("./callbacks/callback"):
-  cfile.write("\t{0}_{1}_handler_t {1}_handler;\n".format(prefix, element.text))
-cfile.write("\t\n")
-for element in root.findall("./fields/field"):
-  cfile.write("\t{0} {1};\n".format(element.attrib['type'], element.text))
-for element in root.findall("./fields/field_decl"):
-  cfile.write("\t{0} {1};\n".format(element.attrib['type'], element.text))
-cfile.write("};\n\n")
-
-
-cfile.write("static {0}_state_t {0}_next_state_given_event({0}_t *machine, {0}_event_t event);\n".format(prefix))
-cfile.write("static void {0}_on_enter({0}_t *machine);\n".format(prefix))
-cfile.write("static void {0}_on_exit({0}_t *machine);\n\n".format(prefix))
-
-
-# Create function
-args = ", ".join([e.attrib['type'] + e.text for e in root.findall("./fields/field")])
-cfile.write("{0}_t *{0}_create({1})\n{{\n".format(prefix, args))
-cfile.write("\t{0}_t *machine = new {0}_t();\n\n".format(prefix))
-cfile.write("\tmachine->state = {0};\n".format(root.findall("initial")[0].text))
-cfile.write("\tmachine->payload = NULL;\n\n")
-for element in root.findall("./fields/field"):
-  cfile.write("\tmachine->{0} = {0};\n".format(element.text))
-cfile.write("\n")
-for element in root.findall("./fields/field_decl"):
-  if 'init' in element.attrib:
-    cfile.write("\tmachine->{0} = {1};\n".format(element.text, element.attrib['init']))
-cfile.write("\n")
-for element in root.findall("./callbacks/callback"):
-  cfile.write("\tmachine->{0}_handler = NULL;\n".format(element.text))
-cfile.write("}\n\n")
-
-
-# Destroy function
-cfile.write("void {0}_destroy({0}_t **machine)\n{{\n".format(prefix))
-cfile.write("\tfree(*machine);\n")
-cfile.write("\t*machine = NULL;\n")
-cfile.write("}\n\n")
-
-
-# Active state
-cfile.write("{0}_state_t {0}_active_state({0}_t *machine)\n{{\n".format(prefix))
-cfile.write("\tassert(machine);\n")
-cfile.write("\treturn machine->state;\n")
-cfile.write("}\n\n")
-
-
-# Callback payload
-cfile.write("void {0}_set_callback_payload({0}_t *machine, void *payload)\n{{\n".format(prefix))
-cfile.write("\tmachine->payload = payload;\n")
-cfile.write("}\n\n")
-
-for element in root.findall("./callbacks/callback"):
-  cfile.write("void {0}_set_{1}_handler({0}_t *machine, {0}_{1}_handler_t handler)\n{{\n".format(prefix, element.text))
-  cfile.write("\tmachine->{0}_handler = handler;\n".format(element.text))
-  cfile.write("}\n\n")
-
-
-# State name
-cfile.write("const char *{0}_statename({0}_state_t state)\n{{\n".format(prefix))
-cfile.write("\tswitch(state) {\n")
-for element in root.findall("./states/state"):
-  cfile.write("\t\tcase {0}: return \"{0}\";\n".format(element.text))
-cfile.write("\t}\n")
-cfile.write("\treturn \"Invalid state\";\n")
-cfile.write("}\n\n")
-
-
-# Event name
-cfile.write("const char *{0}_eventname({0}_event_t event)\n{{\n".format(prefix))
-cfile.write("\tswitch(event) {\n")
-for element in root.findall("./events/event"):
-  cfile.write("\t\tcase {0}: return \"{0}\";\n".format(element.text))
-cfile.write("\t}\n")
-cfile.write("\treturn \"Invalid event\";\n")
-cfile.write("}\n\n")
-
-
-# Handle event
-cfile.write("void {0}_handle_event({0}_t *machine, {0}_event_t event)\n{{\n".format(prefix))
-cfile.write("\tassert(machine);\n")
-cfile.write("\t{0}_state_t next_state = {0}_next_state_given_event(machine, event);\n".format(prefix))
-cfile.write("\tif(!(machine->state == next_state)) {\n")
-cfile.write("\t\t{0}_on_exit(machine);\n".format(prefix))
-cfile.write("\t\tmachine->state = next_state;\n")
-cfile.write("\t\tsyslog(LOG_DEBUG, \"%s() state changed to %s\",  __FUNCTION__, {0}_statename(machine->state));\n".format(prefix))
-cfile.write("\t\t{0}_on_enter(machine);\n".format(prefix))
-cfile.write("\t}\n")
-cfile.write("}\n\n")
 
 
